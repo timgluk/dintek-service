01e5f6acea3fb6853c181b6f1985ec6dfc36a17f
timi-boy <timi.biz@bk.ru>
2022-10-12 17:40:53 +0400

    add: pop-up modal

---
+++ app/js/modal.js
@@ -0,0 +1,15 @@
+const buttonModal = [...document.querySelectorAll('.contacts__button-wrapper__button')];
+const modal = document.querySelector('.modal-wrapper');
+
+buttonModal.forEach((button) => {
+  button.onclick = (e) => {
+    e.preventDefault();
+    modal.classList.add('modal-wrapper_active');
+  };
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal-wrapper') || e.target.classList.contains('modal__close')) {
+    modal.classList.remove('modal-wrapper_active');
+  }
+});
